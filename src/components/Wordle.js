b4a7e74d0caa0231cc4001467eeb102f6a8c386b
rjservidor04 <73457698+rjservidor04@users.noreply.github.com>
2022-11-13 18:49:31 +0800

    Added Coloring to Keys

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -4,7 +4,7 @@ import Grid from './Grid'
 import Keypad from './Keypad'
 
 export default function Wordle({ solution }) {
-    const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution)
+    const { turn, currentGuess, usedKeys, guesses, isCorrect, handleKeyup } = useWordle(solution)
     
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup)
@@ -21,7 +21,7 @@ export default function Wordle({ solution }) {
             <div>Solution = {solution}</div>
             <div>Current Guess = {currentGuess}</div>
             <Grid currentGuess = {currentGuess} guesses = {guesses} turn = {turn} />
-            <Keypad />
+            <Keypad usedKeys = {usedKeys} />
         </div>
     )
 }
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -6,6 +6,7 @@ const useWordle = (solution) => {
     const [guesses, setGuesses] = useState([...Array(6)]) // each guess is an array
     const [history, setHistory] = useState([]) // each guess is a string
     const [isCorrect, setIsCorrect] = useState(false)
+    const [usedKeys, setUsedKeys] = useState({}) // {a : 'green', b : 'yellow', c : 'grey'}
 
     //format a guess into array of letter objects
     //e.g. [{key : 'a', color : 'yellow'}]
@@ -56,6 +57,31 @@ const useWordle = (solution) => {
             return prevTurn + 1
         })
 
+        setUsedKeys((prevUsedKeys) => {
+            let newKeys = {...prevUsedKeys}
+
+            formattedGuess.forEach((l) => {
+                const currentColor = newKeys[l.key]
+
+                if(l.color === 'green') {
+                    newKeys[l.key] = 'green'
+                    return
+                }
+
+                if(l.color === 'yellow' && currentColor !== 'green') {
+                    newKeys[l.key] = 'yellow'
+                    return
+                }
+
+                if(l.color === 'grey' && currentColor !== 'yellow' && currentColor !== 'green') {
+                    newKeys[l.key] = 'grey'
+                    return
+                }
+            })
+
+            return newKeys
+        })
+
         setCurrentGuess('')
     }
 
@@ -98,7 +124,7 @@ const useWordle = (solution) => {
         }
     }
 
-    return {turn, currentGuess, guesses, handleKeyup, isCorrect}
+    return {turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup}
 }
 
 export default useWordle
