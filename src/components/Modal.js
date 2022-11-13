8aa7908898b8c77c129b6197aa00ed30c4819e6d
rjservidor04 <73457698+rjservidor04@users.noreply.github.com>
2022-11-13 19:39:37 +0800

    Added Notification Modal

---
+++ src/components/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Modal( { isCorrect, turn, solution } ) {
+  return (
+    <div className = 'modal'>
+        {isCorrect && (
+            <div>
+                <h1>Congrats, You Win!</h1>
+                <p className = 'solution'>{solution}</p>
+                <p>You found the solution in {turn} guesses</p>
+            </div>
+        )}
+
+        !{isCorrect && (
+            <div>
+                <h1>Sorry, Better Luck Next Time :)</h1>
+                <p className = 'solution'>The word is {solution}</p>
+            </div>
+        )}
+    </div>
+  )
+}
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,21 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 import Grid from './Grid'
 import Keypad from './Keypad'
+import Modal from './Modal'
 
 export default function Wordle({ solution }) {
     const { turn, currentGuess, usedKeys, guesses, isCorrect, handleKeyup } = useWordle(solution)
-    
+    const [showModal, setShowModal] = useState(false)
+
     useEffect(() => {
         window.addEventListener('keyup', handleKeyup)
 
         if(isCorrect) {
-            console.log("Congrats, you win!")
+            setTimeout(() => setShowModal(true), 2000)
             window.removeEventListener('keyup', handleKeyup)
         }
 
         if(turn > 5) {
-            console.log("Sorry, you lost")
+            setTimeout(() => setShowModal(true), 2000)
             window.removeEventListener('keyup', handleKeyup)
         }
 
@@ -29,6 +31,7 @@ export default function Wordle({ solution }) {
             <div>Current Guess = {currentGuess}</div>
             <Grid currentGuess = {currentGuess} guesses = {guesses} turn = {turn} />
             <Keypad usedKeys = {usedKeys} />
+            {showModal && <Modal isCorrect = {isCorrect} turn = {turn} solution = {solution} />}
         </div>
     )
 }
